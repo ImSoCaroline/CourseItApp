c5d2c8e3a20409ae98f03a31cf03b573f3b0d14c
Caro <pastorecarolinae@gmail.com>
2020-08-24 22:10:12 -0300

    destructuring

--- src/components/Sum/index.js
+++ src/components/Sum/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
 
 class Sum extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstNumber: 0,
+      secondNumber: 0,
+      sum: 0
+    }
+  }
+
+  handlechange(e) {
+    const { name, value } = e.target;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleClick() {
+    const { firstNumber, secondNumber } = this.state;
+
+    this.setState({
+      sum: +firstNumber + +secondNumber
+    });
+  }
+
   render() {
-    return <h1>Hola</h1>
+    const { sum } = this.state;
+    return (
+      <>
+        <input name='firstNumber' type='text' onChange={(e) => this.handlechange(e)}></input>
+        <input name='secondNumber' type='text' onChange={(e) => this.handlechange(e)}></input>
+        <button onClick={() => this.handleClick()}>Sumar</button>
+        <p>La suma es: {sum}</p>
+      </>
+    )
   }
 }
 
-export default Sum;
+export default Sum
